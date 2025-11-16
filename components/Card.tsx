29d89357b8250e3109f7f4b880067cb0e4850e8a
jaymoh001-gif <jamesmburu388@gmail.com>
2025-11-16 11:57:13 +0300

    Add Card.tsx component

---
+++ components/Card.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Card: React.FC = () => {
+  return (
+    <div className="border p-4 rounded shadow-md">
+      <p>Card Component</p>
+    </div>
+  );
+};
+
+export default Card;
