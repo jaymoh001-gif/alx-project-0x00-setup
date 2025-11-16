b54b3cfd61ad4f54ba50ffbf1fff76e9d9470358
jaymoh001-gif <jamesmburu388@gmail.com>
2025-11-16 16:08:34 +0300

    Fix project setup, configure TypeScript, add landing and about pages

---
+++ pages/landing.tsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+
+const Landing: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-xl font-extralight">Landing Page</h1>
+    </div>
+  );
+};
+
+export default Landing;
